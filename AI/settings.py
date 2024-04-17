82a1cb4a020e67afff2a696c7dc3bc0958eef4be
tusharbhatiav3 <tushar.bhatiav@vector3.digital>
2024-04-17 16:25:42 +0530

    changes to settings.py-removed static command

--- AI/settings.py
+++ AI/settings.py
@@ -131,10 +131,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-import os
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
