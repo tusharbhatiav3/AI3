8571e3e43537f1de7c76db3f5ab732b8a6abdc70
tusharbhatiav3 <tushar.bhatiav@vector3.digital>
2024-04-17 15:16:23 +0530

    added changes to vercel.json and build_files.sh

--- AI/settings.py
+++ AI/settings.py
@@ -134,7 +134,7 @@ STATIC_URL = 'static/'
 import os
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
 
-#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
